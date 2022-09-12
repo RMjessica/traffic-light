3d0b1fbd806309113d34233098a794ff4a283d00
RMjessica <jeess.rm1@gmail.com>
2022-09-12 21:35:26 +0000

    Add functionalities
    - Automatic lightning
    - One bulb lights up at a time

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,40 +2,48 @@ import React, { useState } from "react";
 import "./../../styles/home.css";
 
 const Home = () => {
-	const [color, setColor] = useState({
-		"red": "off",
-		"yellow": "off",
-		"green": "off",
-		"purple": "off"
-	})   
+	const [color, setColor] = useState([
+		"red",
+		"yellow",
+		"green"
+	]) 
+
+	const [ActiveColor, setActiveColor] = useState("");
 	const [isActive, setActive] = useState(false);
 
+	const timer = ms => new Promise(res => setTimeout(res, ms))
+
   const handlePurple = () => {
-    setActive(!isActive);
+		if (!isActive) {
+			setColor([...color, "purple"]);
+		} else {
+			setColor(color.filter((val) => val !== "purple"));
+		}
+		setActive(!isActive);
   };
 
-	const handleColor = (c) => {
-		let updateColor = {};
-		updateColor[c] = color[c] == "off" ? "on" : "off";
 
-		setColor({
-			...color,
-			...updateColor
-		})
+	 async function auto() {
+	 			for (let c of color) {
+					setActiveColor(c);
+					await timer(1000);
+				}
+				setActiveColor("");
 	}
 
   return (
     <div className="text-center">
 			<div className="stick text-align-center"></div>
 				<div className="box p-2" style={{ height: isActive ? "440px" : "330px"}}>
-					<div className={`bulb ${color["red"] == "on" ? "red" : "off"}`} onClick={() => handleColor("red")} />
-					<div className={`bulb my-2 ${color["yellow"] == "on" ? "yellow" : "off"}`} onClick={() => handleColor("yellow")} />
-					<div className={`bulb ${color["green"] == "on" ? "green" : "off"}`} onClick={() => handleColor("green")} />
-					<div className={`bulb my-1 mt-2 ${isActive ? color["purple"] == "on" ? "purple" : "off": "visually-hidden"}`} onClick={() => handleColor("purple")} />
+
+					<div className={`bulb ${ActiveColor == "red" ? "red" : "off"}`} onClick={() => setActiveColor("red")} />
+					<div className={`bulb my-2 ${ActiveColor == "yellow" ? "yellow" : "off"}`} onClick={() => setActiveColor("yellow")} />
+					<div className={`bulb ${ActiveColor == "green" ? "green" : "off"}`} onClick={() => setActiveColor("green")} />
+					<div className={`bulb my-1 mt-2 ${isActive ? ActiveColor == "purple" ? "purple" : "off" : "visually-hidden"}`} onClick={() => setActiveColor("purple")} />
+
 				</div>
 			<button className="btn btn-outline-dark m-auto mt-3 me-3" onClick={handlePurple} >Add/Remove light</button>
-			<button className="btn btn-outline-dark m-auto mt-3" onClick={handlePurple} >Automatize</button>
-
+			<button className="btn btn-outline-dark m-auto mt-3" onClick={auto} >Automatize</button>
     </div>
   );
 };
