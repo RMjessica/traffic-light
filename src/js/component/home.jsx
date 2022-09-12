eaaac459ef5f69942e01e3e204935e6b2834aff6
RMjessica <jeess.rm1@gmail.com>
2022-09-12 12:48:08 +0000

    Set up
    - Add structure & styles
    - Add onClick glow functionality for bulbs
    - Add new light button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "./../../styles/home.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [color, setColor] = useState({
+		"red": "off",
+		"yellow": "off",
+		"green": "off",
+		"purple": "off"
+	})   
+	const [isActive, setActive] = useState(false);
+
+  const handlePurple = () => {
+    setActive(!isActive);
+  };
+
+	const handleColor = (c) => {
+		let updateColor = {};
+		updateColor[c] = color[c] == "off" ? "on" : "off";
+
+		setColor({
+			...color,
+			...updateColor
+		})
+	}
+
+  return (
+    <div className="text-center">
+			<div className="stick text-align-center"></div>
+				<div className="box p-2" style={{ height: isActive ? "440px" : "330px"}}>
+					<div className={`bulb ${color["red"] == "on" ? "red" : "off"}`} onClick={() => handleColor("red")} />
+					<div className={`bulb my-2 ${color["yellow"] == "on" ? "yellow" : "off"}`} onClick={() => handleColor("yellow")} />
+					<div className={`bulb ${color["green"] == "on" ? "green" : "off"}`} onClick={() => handleColor("green")} />
+					<div className={`bulb my-1 mt-2 ${isActive ? color["purple"] == "on" ? "purple" : "off": "visually-hidden"}`} onClick={() => handleColor("purple")} />
+				</div>
+			<button className="btn btn-outline-dark m-auto mt-3 me-3" onClick={handlePurple} >Add/Remove light</button>
+			<button className="btn btn-outline-dark m-auto mt-3" onClick={handlePurple} >Automatize</button>
+
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,6 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+import Home from "./component/home.jsx";
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
